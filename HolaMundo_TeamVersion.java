767e35fa628cd27c2ea29c74da9eb29b14d3df0c
Ermes Liriano <ermesliriano@mercedarios.net>
2023-08-29 16:05:53 +0200

    Update HolaMundo_TeamVersion.java
    He añadido un comentario con el objetivo de probar cómo se realizan las modificaciones de los ficheros alojados en GitHub.

--- HolaMundo_TeamVersion.java
+++ HolaMundo_TeamVersion.java
@@ -2,5 +2,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola, Juanjo!...");
 		System.out.println("Este es un nuevo intento... Me saludas?");
+		//Este comentario ha sido añadido desde GitHub, después del primer commit
 	}
 }
